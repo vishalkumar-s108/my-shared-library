7f27f59f52d04eaf6cc5b00235ca6c11105bf8a9
vishal kumar <141304370+vishalkumar-s108@users.noreply.github.com>
2026-07-01 17:26:37 +0530

    Enhance database connection logging and security

--- vars/connectDatabase.groovy
+++ vars/connectDatabase.groovy
@@ -1,7 +1,14 @@
 // vars/connectDatabase.groovy
 def call(String projectName) {
     withCredentials([string(credentialsId: 'my-db-pass', variable: 'DB_PASSWORD')]) {
-        echo "=== [LIBRARY] Starting database logic for ${projectName} ==="
-        sh "echo 'Connecting to DB with masked password: ${DB_PASSWORD}'"
+        
+        // Naya updated aur clean message logs ke liye
+        echo "================================================================"
+        echo "🔄 [DATABASE INFO] Initiating Secure Connection for: ${projectName.toUpperCase()}"
+        echo "🔐 [SECURITY] Fetching credentials from Jenkins Credential Provider..."
+        echo "================================================================"
+        
+        // Masked password ke sath secure shell command
+        sh "echo '🚀 Status: Successfully established connection to the database for ${projectName}! Password is safe and masked.'"
     }
 }
