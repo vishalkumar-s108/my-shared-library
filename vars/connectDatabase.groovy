2e8c5b17815011cf385d1f7d573f56aadcc17b4d
vishal kumar <141304370+vishalkumar-s108@users.noreply.github.com>
2026-07-01 16:12:37 +0530

    Update connectDatabase.groovy

--- vars/connectDatabase.groovy
+++ vars/connectDatabase.groovy
@@ -1,1 +1,7 @@
-
+// vars/connectDatabase.groovy
+def call(String projectName) {
+    withCredentials([string(credentialsId: 'my-db-pass', variable: 'DB_PASSWORD')]) {
+        echo "=== [LIBRARY] Starting database logic for ${projectName} ==="
+        sh "echo 'Connecting to DB with masked password: ${DB_PASSWORD}'"
+    }
+}
